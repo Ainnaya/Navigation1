a12c7080c3cb684f09562de1c06a12320b033b6c
Ainnaya <Ainnaya56@gmail.com>
2017-03-04 15:35:33 +0700

    Swipe Views Change Fragment

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/navigation1/Page2Fragment.java
@@ -0,0 +1,25 @@
+package id.sch.smktelkom_mlg.learn.navigation1;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Page2Fragment extends Fragment {
+
+
+    public Page2Fragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_page2, container, false);
+    }
+
+}
